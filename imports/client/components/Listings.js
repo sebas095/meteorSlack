5473b1b71bd53d117d435c8d84f9eee9f7d6204a
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-06 17:39:24 -0500

    basic structure to listings  component

--- imports/client/components/Listings.js
+++ imports/client/components/Listings.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
+import Channel from './Channel';
+
 export default class Listings extends Component {
   constructor() {
     super();
   }
 
   render() {
+    const channelName = 'channel';
+
     return (
-      <div></div>
+      <div>
+        <Channel name={channelName} />
+        <Channel name={channelName} />
+        <Channel name={channelName} />
+      </div>
     );
   }
 }
