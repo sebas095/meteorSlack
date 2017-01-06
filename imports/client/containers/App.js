04cf6bf790af52dca11d08978deb27034b694bb0
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-06 18:02:56 -0500

    Transform app into component container

--- imports/client/containers/App.js
+++ imports/client/containers/App.js
@@ -1,6 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
 
-export default class App extends Component {
+import AccountUIWrapper from '../components/AccountsUIWrapper';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Listings from '../components/Listings';
+import Messages from '../components/Messages';
+import ProfileModal from '../components/ProfileModal';
+
+class App extends Component {
   constructor() {
     super();
   }
@@ -11,3 +20,17 @@ export default class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  messages: PropTypes.array.isRequired,
+  channels: PropTypes.array.isRequired,
+  currentUser: PropTypes.object
+};
+
+export default createContainer(() => {
+  return {
+    messages: [],
+    channels: [],
+    currentUser: Meteor.user()
+  }
+}, App);
