f17f34b8060d0c92bfddc475f5df450a1f67f31d
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-05 22:36:02 -0500

    first components and containers

--- client/main.js
+++ client/main.js
@@ -2,14 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
-class App extends React.Component {
+class Comment extends React.Component {
   constructor() {
     super();
   }
 
   render() {
     return (
-      <h1>Hola mundo, desde react</h1>
+      <h4>{this.props.comment}</h4>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const comments = [
+      { _id: Date.now(), comment: 'Hola Meteor' },
+      { _id: Date.now() + 1, comment: 'Hola Meteor' },
+      { _id: Date.now() + 2, comment: 'Hola Meteor' }
+    ];
+    const renderComments = comments.map((comment) => {
+      return (<Comment key={comment._id} comment={comment.comment} />);
+    });
+
+    return (
+      <div>
+        {renderComments}
+      </div>
     );
   }
 }
