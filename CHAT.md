36394a52cb9e34ace6d4aa41c446aecc74b70995
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-05 22:23:32 -0500

    hello world from meteor with react

---
+++ CHAT.md
@@ -0,0 +1,12 @@
+|-- Components
+|---- AccountsUIWrapper
+|---- Channel // solo canales
+|---- Footer // input
+|---- Header // nombre de usuario
+|---- Listings // muchos canales
+|---- Message // un mensaje
+|---- Messages // muchos mensajes
+|---- ProfileModal // perfil del usuario
+
+|-- Containers
+|---- App // todos los componentes
--- client/main.js
+++ client/main.js
@@ -1,22 +1,20 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 
-import './main.html';
+class App extends React.Component {
+  constructor() {
+    super();
+  }
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+  render() {
+    return (
+      <h1>Hola mundo, desde react</h1>
+    );
+  }
+}
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  const base = document.getElementById('container');
+  ReactDOM.render(<App/>, base);
 });
