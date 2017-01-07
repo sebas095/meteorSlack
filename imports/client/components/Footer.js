fcb6f68dd4cf6ef9763184fb864a6206b32e20f0
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-06 23:14:32 -0500

    footer styles

--- imports/client/components/Footer.js
+++ imports/client/components/Footer.js
@@ -14,14 +14,21 @@ export default class Footer extends Component {
   }
 
   render() {
+    const padding = {
+      padding: '2%',
+      margin: '1%'
+    };
+
     return (
-      <div>
-        <form className='ui form' onSubmit={this.handleSubmit.bind(this)}>
-          <div className='ui huge icon input'>
-            <input ref="inputMessage" placeholder='Enviar mensaje...' type="text" />
-            <i className='send outline icon'></i>
-          </div>
-        </form>
+      <div className='ui two column grid'>
+        <div style={padding} className='teal row'>
+          <form className='ui form' onSubmit={this.handleSubmit.bind(this)}>
+            <div className='ui huge icon input'>
+              <input ref="inputMessage" placeholder='Enviar mensaje...' type="text" />
+              <i className='send outline icon'></i>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
